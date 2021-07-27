68cfb770339cef2829a5884cb68f54bcd7090766
Yaroslav Muravskyi <31-yaroslav.m@users.noreply.gitlab.host1plus.com>
2021-07-27 17:58:11 +0200

    install plugins

--- .zsh
+++ .zsh
@@ -11,4 +11,11 @@ brew install romkatv/powerlevel10k/powerlevel10k
 echo 'source /usr/local/opt/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >>~/.zshrc
 
+rm -rf ~/.p10k.zsh
 ln -s ~/.dotfile/init/.p10k.zsh ~/.p10k.zsh
+
+# zsh-autosuggestions plugin
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# zsh-syntax-highlighting plugin
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
